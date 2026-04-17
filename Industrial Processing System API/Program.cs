0bafb258c5d8820e0c2fcd3aef1125f91788fd28
nadjalucic <lucic.sv50.2023@uns.ac.rs>
2026-04-17 10:42:26 +0200

    Add initial project structure

---
+++ Industrial Processing System API/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Industrial_Processing_System_API
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
